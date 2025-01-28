6712d560727fe78010ea66036eba0adb33ac47a5
VictorJSSantos <victormvll@gmail.com>
2025-01-28 00:50:04 -0300

    Ajustes na formatação dos valores monetários.

--- desafio.py
+++ desafio.py
@@ -12,7 +12,7 @@ class ContaBancaria:
         if valor > 0:
             self.saldo += valor
             print(
-                f"Valor de R${valor} adicionado ao saldo. Saldo total em R${self.saldo}"
+                f"Valor de R${valor:.2f} adicionado ao saldo. Saldo total em R${self.saldo:.2f}"
             )
 
     def sacar(self, valor):
@@ -25,13 +25,13 @@ class ContaBancaria:
             self.saldo -= valor
             self.quantidade_de_saques_a_realizar -= 1
             print(
-                f"O valor de {valor} foi sacado da conta e agora o saldo é {self.saldo}. Restam {self.quantidade_de_saques_a_realizar} operações de saque."
+                f"O valor de {valor:.2f} foi sacado da conta e agora o saldo é {self.saldo:.2f}. Restam {self.quantidade_de_saques_a_realizar} operações de saque."
             )
         elif valor > 500:
-            print(f"O valor do saque {valor} é maior que o permitido de R$500")
+            print(f"O valor do saque {valor:.2f} é maior que o permitido de R$500.00")
         elif self.saldo - valor <= 0:
             print(
-                f"O seu saldo é insuficiente para sacar. Saldo de R${self.saldo} e tentativa de saque de R${valor}"
+                f"O seu saldo é insuficiente para sacar. Saldo de R${self.saldo:.2f} e tentativa de saque de R${valor:.2f}"
             )
         else:
             print(f"Você não possui mais operações de saque a realizar.")
@@ -39,7 +39,7 @@ class ContaBancaria:
     def consultar_extrato(self):
         extrato = f"""
     A sua conta contém {self.quantidade_de_saques_a_realizar} operações de saques para realizar.
-    O saldo total é R${self.saldo}
+    O saldo total é R${self.saldo:.2f}
 """
         print(extrato)
 
